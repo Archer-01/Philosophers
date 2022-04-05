13292d20295ddfb61131d9ba4534c22bd1e8d787
Hamza <hhamza@student.1337.ma>
2022-04-05 09:46:51 -0400

    Add ft_semaphore
    Add semaphore interface (ft_semaphore)
    Semaphore interface functions are as follows:
    - ft_sem_open
    - ft_sem_close
    - ft_sem_unlink
    - ft_sem_post
    - ft_sem_wait
    Add semaphore error message macros to philosophers.h
    Add ft_semaphore functions prototypes to philosophers.h

--- include/philosophers.h
+++ include/philosophers.h
@@ -6,7 +6,7 @@
 /*   By: hhamza <hhamza@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/20 04:54:44 by hhamza            #+#    #+#             */
-/*   Updated: 2022/04/04 14:15:21 by hhamza           ###   ########.fr       */
+/*   Updated: 2022/04/05 09:39:46 by hhamza           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,6 +32,11 @@
 # define E_INVAL_MSG "Error. Invalid arguments"
 # define E_PHILOCOUNT_MSG "Error. philosophers count must not exceed 200"
 # define E_TIME_MSG "Error. time to die/sleep/eat must be 60ms or above"
+# define E_SEM_OPEN_MSG "Error. failed to open/create semaphore"
+# define E_SEM_CLOSE_MSG "Error. failed to close semaphore"
+# define E_SEM_POST_MSG "Error. failed to post semaphore"
+# define E_SEM_WAIT_MSG "Error. failed to wait semaphore"
+# define E_SEM_UNLINK_MSG "Error. failed to unlink semaphore"
 
 typedef enum e_bool
 {
@@ -56,5 +61,11 @@ int				ft_atoi(const char *str);
 void			*ft_calloc(size_t count, size_t size);
 void			*ft_allocate(size_t count, size_t size);
 t_philo_args	*ft_parse_philo_args(int argc, char **argv);
+
+sem_t			*ft_sem_open(const char *name, unsigned int value);
+t_bool			ft_sem_close(sem_t *sem);
+t_bool			ft_sem_unlink(const char *name);
+t_bool			ft_sem_post(sem_t *sem);
+t_bool			ft_sem_wait(sem_t *sem);
 
 #endif
---
+++ src/ft_semaphore.c
@@ -0,0 +1,145 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_semaphore.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hhamza <hhamza@student.1337.ma>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/05 09:18:07 by hhamza            #+#    #+#             */
+/*   Updated: 2022/04/05 09:38:28 by hhamza           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philosophers.h"
+
+/**
+ * @brief Open a named semaphore.
+ * Also create semaphore if it doesn't exist
+ *
+ * @param name: semaphore name
+ * @param value: semaphore initial value
+ * @return sem_t*: semaphore object, NULL on failure
+ */
+sem_t	*ft_sem_open(const char *name, unsigned int value)
+{
+	sem_t	*sem;
+
+	if (name == NULL)
+	{
+		return (NULL);
+	}
+	sem = sem_open(name, O_CREAT, 0600, value);
+	if (sem == SEM_FAILED)
+	{
+		ft_putendl_fd(E_SEM_OPEN_MSG, STDERR_FILENO);
+		return (NULL);
+	}
+	else
+	{
+		return (sem);
+	}
+}
+
+/**
+ * @brief Close a names semaphore
+ *
+ * @param sem: semaphore object
+ * @return t_bool: TRUE on success, FALSE otherwise
+ */
+t_bool	ft_sem_close(sem_t *sem)
+{
+	int	ret;
+
+	if (sem == NULL)
+	{
+		return (FALSE);
+	}
+	ret = sem_close(sem);
+	if (ret == -1)
+	{
+		ft_putendl_fd(E_SEM_CLOSE_MSG, STDERR_FILENO);
+		return (FALSE);
+	}
+	else
+	{
+		return (TRUE);
+	}
+}
+
+/**
+ * @brief Unlink (Remove) a named semaphore
+ *
+ * @param name: semaphore name
+ * @return t_bool: TRUE on success, FALSE otherwise
+ */
+t_bool	ft_sem_unlink(const char *name)
+{
+	int	ret;
+
+	if (name == NULL)
+	{
+		return (FALSE);
+	}
+	ret = sem_unlink(name);
+	if (ret == -1)
+	{
+		ft_putendl_fd(E_SEM_UNLINK_MSG, STDERR_FILENO);
+		return (FALSE);
+	}
+	else
+	{
+		return (TRUE);
+	}
+}
+
+/**
+ * @brief Post (Unlock) a semaphore
+ *
+ * @param sem: semaphore object
+ * @return t_bool: TRUE on success, FALSE otherwise
+ */
+t_bool	ft_sem_post(sem_t *sem)
+{
+	int	ret;
+
+	if (sem == NULL)
+	{
+		return (FALSE);
+	}
+	ret = sem_post(sem);
+	if (ret == -1)
+	{
+		ft_putendl_fd(E_SEM_POST_MSG, STDERR_FILENO);
+		return (FALSE);
+	}
+	else
+	{
+		return (TRUE);
+	}
+}
+
+/**
+ * @brief Wait (Lock) semaphore
+ *
+ * @param sem: semaphore object
+ * @return t_bool: TRUE on success, FALSE otherwise
+ */
+t_bool	ft_sem_wait(sem_t *sem)
+{
+	int	ret;
+
+	if (sem == NULL)
+	{
+		return (FALSE);
+	}
+	ret = sem_wait(sem);
+	if (ret == -1)
+	{
+		ft_putendl_fd(E_SEM_WAIT_MSG, STDERR_FILENO);
+		return (FALSE);
+	}
+	else
+	{
+		return (TRUE);
+	}
+}

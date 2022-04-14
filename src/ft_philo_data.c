c094a72555aa5b50db596d5be14e7cf0a14ca35e
Hamza <hhamza@student.1337.ma>
2022-04-14 17:50:49 +0000

    Fix memory leak on ft_init_philo_data
    Free philo_data on error

--- src/ft_philo_data.c
+++ src/ft_philo_data.c
@@ -6,7 +6,7 @@
 /*   By: hhamza <hhamza@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/05 11:18:03 by hhamza            #+#    #+#             */
-/*   Updated: 2022/04/13 12:15:18 by hhamza           ###   ########.fr       */
+/*   Updated: 2022/04/14 17:48:31 by hhamza           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,19 +41,16 @@ t_philo_data	*ft_init_philo_data(int argc, char **argv)
 		return (NULL);
 	philo_data = ft_allocate(1, sizeof(t_philo_data));
 	if (philo_data == NULL)
-	{
-		free(philo_args);
-		return (NULL);
-	}
+		return (free(philo_args), NULL);
 	philo_data->philo_args = philo_args;
 	philo_data->fork_sem = ft_sem_open(FORK_SEM_NAME, philo_args->philo_count);
 	if (philo_data->fork_sem == SEM_FAILED)
-		return (free(philo_args), NULL);
+		return (free(philo_args), free(philo_data), NULL);
 	philo_data->writing_sem = ft_sem_open(WRITE_SEM_NAME, 1);
 	if (philo_data->fork_sem == SEM_FAILED)
-		return (free(philo_args), NULL);
+		return (free(philo_args), free(philo_data), NULL);
 	if (ft_gettimeofday(&philo_data->begin_time) == FALSE)
-		return (free(philo_args), NULL);
+		return (free(philo_args), free(philo_data), NULL);
 	return (philo_data);
 }
 
